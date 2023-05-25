e7f1e3fd895eb9e322482c49de29db5f7df4c48f
Intuitive Harmony <j.horst77@gmail.com>
2023-05-24 22:51:21 -0600

    prompt 10 - response

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ const Box = ({ size }) => {
         width: `${size}px`,
         height: `${size}px`,
         margin: "1px",
+        flex: "1 0 0",
       }}
     />
   );
@@ -36,11 +37,15 @@ const BoxContainer = ({ numBoxes, boxSize }) => {
   const boxes = Array.from({ length: numBoxes }).map((_, i) => (
     <Box key={i} size={boxSize} />
   ));
-  return <div style={{ display: "flex", flexWrap: "wrap" }}>{boxes}</div>;
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", height: "100vh" }}>
+      {boxes}
+    </div>
+  );
 };
 
 export default function App() {
   return <BoxContainer numBoxes={400} boxSize={8} />;
 }
 
-// Base code
+// I added the flex: '1 0 0' property to the Box component's style to allow the boxes to take up equal space within their container. I also added a height: '100vh' property to the BoxContainer's style to make it take up the full height of the viewport.
