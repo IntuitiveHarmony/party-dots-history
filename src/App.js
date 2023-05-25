4ba6d1d0a94ecca218e2acbf0811b3d7120cb13b
Intuitive Harmony <j.horst77@gmail.com>
2023-05-24 20:17:48 -0600

    prompt 8

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react";
 
 const Box = ({ size }) => {
   const [color, setColor] = useState(getRandomColor());
+  const [intervalId, setIntervalId] = useState(null);
 
   useEffect(() => {
-    const intervalId = setInterval(
+    const id = setTimeout(
       () => setColor(getRandomColor()),
       Math.random() * 5000
     );
+    setIntervalId(id);
     return () => clearInterval(intervalId);
-  }, []);
+  }, [intervalId]);
 
   function getRandomColor() {
     const letters = "0123456789ABCDEF";
@@ -24,9 +26,8 @@ const Box = ({ size }) => {
     <div
       style={{
         backgroundColor: color,
-        width: `${size}px`,
-        height: `${size}px`,
-        margin: "1px",
+        width: "100%",
+        height: "100%",
       }}
     />
   );
@@ -36,11 +37,22 @@ const BoxContainer = ({ numBoxes, boxSize }) => {
   const boxes = Array.from({ length: numBoxes }).map((_, i) => (
     <Box key={i} size={boxSize} />
   ));
-  return <div style={{ display: "flex", flexWrap: "wrap" }}>{boxes}</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      {boxes}
+    </div>
+  );
 };
 
 export default function App() {
   return <BoxContainer numBoxes={400} boxSize={8} />;
 }
 
-// In this version, the useEffect hook in the Box component now has an empty dependency array ([]), which means it will only run once when the component mounts, instead of running on every re-render. This should prevent the infinite loop and the Maximum update depth exceeded error.
+// In this version, the BoxContainer component now has a height of 100vh and a width of 100%, which should make it take up the entire viewport. The Box component now has a width and height of 100%, which should make it fill up the entire BoxContainer.
