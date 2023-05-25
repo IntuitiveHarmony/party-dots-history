13e9543d791a4456136d26550469d043010354c5
Intuitive Harmony <j.horst77@gmail.com>
2023-05-24 23:31:41 -0600

    prompt 23

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 const Box = ({ size }) => {
   const [color, setColor] = useState(getRandomColor());
@@ -35,61 +35,23 @@ const Box = ({ size }) => {
 };
 
 const BoxContainer = ({ numBoxes, boxSize }) => {
-  const boxContainerRef = useRef(null);
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const boxCount = Math.floor(width / boxSize);
-  const rows = Math.ceil(numBoxes / boxCount);
-  const totalBoxes = boxCount * rows;
-  const boxes = Array.from({ length: totalBoxes }).map((_, i) => (
+  const boxes = Array.from({ length: numBoxes }).map((_, i) => (
     <Box key={i} size={boxSize} />
   ));
-
-  const containerWidth = boxCount * boxSize;
-  const containerHeight = rows * boxSize;
-
   return (
     <div
-      ref={boxContainerRef}
       style={{
         display: "flex",
         flexWrap: "wrap",
-        width: "100vw",
-        height: "100vh",
         margin: "0",
-        padding: "0",
-        overflow: "hidden",
-        justifyContent: "center",
-        alignItems: "center",
+        height: "100vh",
       }}
     >
-      <div
-        style={{
-          width: `${containerWidth}px`,
-          height: `${containerHeight}px`,
-          margin: "0",
-          padding: "0",
-          boxSizing: "border-box",
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {boxes.slice(0, numBoxes)}
-      </div>
+      {boxes}
     </div>
   );
 };
 
 export default function App() {
-  return <BoxContainer numBoxes={400} boxSize={20} />;
+  return <BoxContainer numBoxes={400} boxSize={10} />;
 }
-
-// This code should automatically adjust the number of boxes per row based on the width of the screen, and make sure that the boxes cover the whole screen without any gaps.
