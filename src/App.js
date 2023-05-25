a7e5ca2bb952e318e82d8d87f6f08301f99b5e9c
Intuitive Harmony <j.horst77@gmail.com>
2023-05-24 20:08:14 -0600

    prompt 6 with error comment

--- src/App.js
+++ src/App.js
@@ -44,3 +44,9 @@ const BoxContainer = ({ numBoxes, boxSize }) => {
 export default function App() {
   return <BoxContainer numBoxes={400} boxSize={8} />;
 }
+
+// Me - it keeps throwing this error:
+// Warning: Maximum update depth exceeded. This can happen when a component calls setState inside useEffect, but useEffect either doesn't have a dependency array, or one of the dependencies changes on every render. at Box (http://localhost:3000/static/js/bundle.js:28:5) at div at BoxContainer (http://localhost:3000/static/js/bundle.js:62:5) at App
+
+// GPT - This error occurs when the component gets stuck in an infinite loop of rendering, and it's usually caused by a state update that triggers a re-render, which then triggers another state update, and so on. In your case, it looks like the useEffect hook inside the Box component is causing the issue.
+// GPT - To fix this, you need to pass an empty dependency array ([]) to the useEffect hook, so that it only runs once when the component mounts, instead of running on every re-render.
