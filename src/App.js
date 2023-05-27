179b70abf2a0ad7b28b329716289269d266c456b
Intuitive Harmony <j.horst77@gmail.com>
2023-05-26 22:13:09 -0600

    prompt 29

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const Box = ({ size }) => {
     <div
       style={{
         backgroundColor: color,
+        borderRadius: "30px",
         width: `${size}%`,
         paddingBottom: `${size}%`,
         float: "left",
@@ -61,7 +62,3 @@ export default function App() {
 
   return start ? <BoxContainer numBoxes={400} boxSize={4} /> : null;
 }
-
-// In this version, we've added a new state variable called start, which is initially set to false. We also added an useEffect hook that waits for a second (using setTimeout) before setting start to true. The BoxContainer component is now only rendered when start is true.
-
-// By setting the setTimeout delay to 1000ms, we ensure that the first interval runs after the initial delay, giving the app some time to load before starting the animation. We also slightly reduced the interval range to 500-5000ms to minimize the delay a few seconds in.
